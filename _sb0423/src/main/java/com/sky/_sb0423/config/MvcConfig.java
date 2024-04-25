fd77bc5fca319d41c0d63319f96a543a85542308
Joey <1llalwaystryit@gmail.com>
2024-04-25 13:08:09 +0900

    intercetor

--- _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
+++ _sb0423/src/main/java/com/sky/_sb0423/config/MvcConfig.java
@@ -1,13 +1,26 @@
 package com.sky._sb0423.config;
 
+import com.sky._sb0423.interceptor.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("main");
+//   켰을떄 바로 메인이 main으로 시작하게 하라는 뜻
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+//        인터셉터만들고 등록한것
+                .addPathPatterns("/register/**")
+                .excludePathPatterns("/practice/**");
     }
 }
--- _sb0423/src/main/java/com/sky/_sb0423/interceptor/LoggerInterceptor.java
+++ _sb0423/src/main/java/com/sky/_sb0423/interceptor/LoggerInterceptor.java
@@ -1,13 +1,25 @@
 package com.sky._sb0423.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Slf4j
 public class LoggerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        log.info("------------prehandle------------");
+        log.info("Request URI \t{}", request.getRequestURI()); // 콘솔에 uri 정보가 나타난다
         return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+//    post는 return이 없음 필터랑 쓰는방법 거의 비슷함 / 끝나기 때문에 돌려줄게 없어서 리턴이 없음
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("------------posthandle------------");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 }
